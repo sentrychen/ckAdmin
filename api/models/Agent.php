315bffd2eb9a95a8b022a218a9984c8d0aebf3ac
onetop <onetop.pw@gmail.com>
2018-11-16 17:36:35 +0800

    添加api postman文档

--- api/models/Agent.php
+++ api/models/Agent.php
@@ -16,12 +16,22 @@ class Agent extends \common\models\Agent
     public function beforeSave($insert)
     {
         if ($insert) {
-            $option = yii::$app->option;
 
-            $this->rebate_rate = $option->agent_default_rebate;
-            $this->xima_status = $option->agent_xima_status;
-            $this->xima_type = $option->agent_xima_type;
-            $this->xima_rate = $option->agent_xima_rate;
+            if ($this->parent_id) {
+                $parent = $this->getParent();
+                $this->rebate_rate = $parent->rebate_rate;
+                $this->xima_status = $parent->xima_status;
+                $this->xima_type = $parent->xima_type;
+                $this->xima_rate = $parent->xima_rate;
+            } else {
+                $option = yii::$app->option;
+
+                $this->rebate_rate = $option->agent_default_rebate;
+                $this->xima_status = $option->agent_xima_status;
+                $this->xima_type = $option->agent_xima_type;
+                $this->xima_rate = $option->agent_xima_rate;
+            }
+
         }
         return parent::beforeSave($insert);
     }
