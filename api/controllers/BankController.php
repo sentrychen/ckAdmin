ea45a40cb889d1001159d5f4752687d0eefc89fc
taiyan <taiyan@qq.com>
2018-11-23 14:12:48 +0800

    钱包功能开发

---
+++ api/controllers/BankController.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Author: ty
+ * Blog: https://blog.feehi.com
+ * Email: job@feehi.com
+ * Created at: 2018-11-23 10:00
+ */
+namespace api\controllers;
+use api\components\RestHttpException;
+use api\models\UserBank;
+use Yii;
+
+
+class BankController extends ActiveController
+{
+    public $modelClass = "api\models\User";
+
+    public function actions()
+    {
+        return [];
+    }
+
+    public function actionIndex()
+    {
+        return [
+            "onetop api service"
+        ];
+    }
+
+    /*
+     * 银行卡列表
+     * @return obj
+     *
+     */
+    public function actionList()
+    {
+        $user = Yii::$app->getUser()->getIdentity();
+        $bank = new UserBank();
+        $result = $bank::find()
+            ->where(['user_id' => $user->getId(),'status'=>1])
+            ->orderBy('id')
+            ->all();
+        if($result)
+        {
+            return $result;
+        }
+
+        $errorReasons = $result->getErrors();
+        if (empty($errorReasons)) {
+            throw new RestHttpException();
+        } else {
+            $err = '';
+            foreach ($errorReasons as $errorReason) {
+                $err .= $errorReason[0] . '<br>';
+            }
+            $err = rtrim($err, '<br>');
+            throw new RestHttpException($err, 400);
+        }
+    }
+
+    /*
+     * 新建银行卡
+     * @return obj
+     *
+     */
+    public function actionAdd()
+    {
+        $user = Yii::$app->getUser()->getIdentity();
+        $bank = new UserBank();
+        $bank->user_id = $user->getId();
+        $bank->username = $user->username;
+        $bank->setAttributes(Yii::$app->request->post());
+        if ($bank->save()) {
+            return $bank->toArray();
+        }
+        $errorReasons = $bank->getErrors();
+
+        if (empty($errorReasons)) {
+            throw new RestHttpException();
+        } else {
+            $err = '';
+            foreach ($errorReasons as $errorReason) {
+                $err .= $errorReason[0] . '<br>';
+            }
+            $err = rtrim($err, '<br>');
+            throw new RestHttpException($err, 400);
+        }
+    }
+
+    /*
+     * 编辑银行卡
+     * @return obj
+     *
+     */
+    public function actionEdit()
+    {
+        $user = Yii::$app->getUser()->getIdentity();
+        $request =  Yii::$app->request;
+        $bank = UserBank::findOne($request->post('id'));
+        $bank->user_id = $user->getId();
+        $bank->username = $user->username;
+        $bank->setAttributes($request->post());
+        if ($bank->save()) {
+            return $bank->toArray();
+        }
+        $errorReasons = $bank->getErrors();
+
+        if (empty($errorReasons)) {
+            throw new RestHttpException();
+        } else {
+            $err = '';
+            foreach ($errorReasons as $errorReason) {
+                $err .= $errorReason[0] . '<br>';
+            }
+            $err = rtrim($err, '<br>');
+            throw new RestHttpException($err, 400);
+        }
+    }
+}
---
+++ api/models/UserDeposit.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace api\models;
+
+use Yii;
+
+class UserDeposit extends \common\models\UserDeposit
+{
+    public function rules()
+    {
+        return [
+            [['user_id', 'apply_amount', 'pay_channel','pay_username'], 'required'],
+            [['user_id', 'status', 'audit_by_id', 'audit_at', 'pay_channel', 'save_bank_id', 'feedback', 'feedback_at', 'updated_at', 'created_at'], 'integer'],
+            [['apply_amount', 'confirm_amount'], 'number', 'min' => 0],
+            [['remark', 'audit_remark', 'pay_username', 'pay_nickname', 'pay_info', 'feedback_remark'], 'string', 'max' => 255],
+            [['audit_by_username'], 'string', 'max' => 64],
+        ];
+    }
+}
