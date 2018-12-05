5bd4861472ba8cc1dd6376cda3e4a70e03b003d1
taiyan <taiyan@qq.com>
2018-12-05 16:03:56 +0800

    api添加公司银行卡账户和收款二维码

---
+++ api/models/Upload.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/5
+ * Time: 14:34
+ */
+
+namespace api\models;
+
+
+class Upload extends \common\models\Upload
+{
+
+}
