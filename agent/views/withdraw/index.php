70777d8d0591901255ed6b54a37e5d4cfa8fd98f
taiyan <taiyan@qq.com>
2018-12-18 09:44:56 +0800

    代理各列表金额格式化

--- agent/views/withdraw/index.php
+++ agent/views/withdraw/index.php
@@ -20,6 +20,7 @@ use common\grid\{
 };
 use yii\helpers\Html;
 use yii\helpers\Url;
+use common\helpers\Util;
 ?>
 
 <div class="row">
@@ -54,15 +55,24 @@ use yii\helpers\Url;
                         ],
                         [
                             'attribute' => 'apply_amount',
-                            'format' => 'currency',
+                            'format' => 'raw',
+                            'value' => function($model){
+                                return Util::formatMoney($model->apply_amount,false);
+                            }
                         ],
                         [
                             'attribute' => 'transfer_amount',
-                            'format' => 'currency',
+                            'format' => 'raw',
+                            'value' => function($model){
+                                return Util::formatMoney($model->transfer_amount,false);
+                            }
                         ],
                         [
                             'attribute' => 'agent.account.frozen_amount',
-                            'format' => 'currency',
+                            'format' => 'raw',
+                            'value' => function($model){
+                                return Util::formatMoney(isset($model->transfer_amount)?$model->transfer_amount:0,false);
+                            }
                         ],
                         [
                             'attribute' => 'status',
