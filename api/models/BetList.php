877d6ced824a98055e7e97898addfb89c1d94e29
onetop <onetop.pw@gmail.com>
2019-01-10 18:55:41 +0800

    修改交易记录接口

--- api/models/BetList.php
+++ api/models/BetList.php
@@ -6,5 +6,4 @@ use Yii;
 
 class BetList extends \common\models\BetList
 {
-
 }
--- api/models/UserAccountRecord.php
+++ api/models/UserAccountRecord.php
@@ -2,10 +2,24 @@
 
 namespace api\models;
 
+use common\libs\Constants;
 use Yii;
 
 
 class UserAccountRecord extends \common\models\UserAccountRecord
 {
+    public function fields()
+    {
+        $fields = parent::fields();
+
+        $fields['switch_name'] = function ($model) {
+            return UserAccountRecord::getSwitchStatus($model->switch);
+        };
+        $fields['trade_type'] = function ($model) {
+            $items = Constants::getTradeTypeItems();
+            return $items[$model->trade_type_id] ?? '其它';
+        };
+        return $fields;
+    }
 
 }
